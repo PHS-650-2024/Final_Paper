344c2f97c6c4cc946e2766bbe6aa6c21564cd19f
alaboe <agathalaboe@gmail.com>
2024-04-30 16:57:21 -0500

    scoring, aim 1, and aim 2

---
+++ R/0.Scoring.R
@@ -0,0 +1,43 @@
+# Load necessary packages
+library(readr)
+library(tidyverse)
+
+# Read in the data
+d <- read_csv("data/olympics.csv")
+
+# Create a new, dichotomous "gold" variable from categorical "medal" variable by 
+# recoding “medal” character variable to gold = 1, silver or bronze = 0, and no 
+# medal = NA.
+d$gold <- d$medal
+
+d <- d %>%
+  mutate(across(c(gold),
+                ~dplyr::recode(.,
+                               "Gold" = 1,
+                               "Silver" = 0,
+                               "Bronze" = 0)))
+
+# Create a new, dichotomous "medal_dichotomous" variable, recoding "medal" character
+# variable to gold, silver, or bronze = 1 and NA = 0 
+olympics$medal_dichotomous <- ifelse(!is.na(olympics$medal) & olympics$medal != "NA", 1, 0)
+
+# Create a centered sex variable 
+olympics$sex_c <- dplyr::recode(olympics$sex, "M" = -0.5, "F" = 0.5)
+
+# Subset data to only include years 1994 and onward
+d1994 <- subset(d, year >= 1994 & !is.na(age) & !is.na(weight) & !is.na(id) & !is.na(gold))
+
+# Create a new 'event_gender' column and update 'event' column
+d1994 <- d1994 %>%
+  mutate(event_gender = str_extract(event, "(Men's|Women's|Mixed|\\(Men\\)'s)"),
+         event = str_remove(event, "(Men's|Women's|Mixed|\\(Men\\)'s)"))
+
+# Only include events where gender = mixed for subsetted data of 1994 onward
+d_mixed <- d1994 %>% filter(event_gender == "Mixed")
+
+# Recode gold as factor with levels 1 and 0
+d1994$gold <- factor(d1994$gold, levels = c(1, 0))
+
+# Recode id for grouping by athlete
+d1994$id <- factor(d1994$id) 
+
